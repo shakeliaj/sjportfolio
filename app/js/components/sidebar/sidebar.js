125af1fde6963e7a7131f6d22f7d8546388dcb7a
Shakelia <sjohnson@inginestudio.com>
2017-05-06 19:53:58 -0400

    html and scss changes for the sidebar, as well as incorporating state transition from the anchor (nav item) clicks

--- app/js/components/sidebar/sidebar.js
+++ app/js/components/sidebar/sidebar.js
@@ -1,9 +1,22 @@
 angular.module('sjportfolio')
-  .controller('sidebarController', SidebarController);
+    .controller('SidebarController', SidebarController);
 
 //include any dependencies inside the parentheses
-  function SidebarController($state) {
+function SidebarController($state, $location) {
     var sideCtrl = this;
 
+    //list of dropdown items
+    sideCtrl.dropdownList = ['Home', 'About', 'Portfolio', 'Resume', 'Contact'];
 
-  }
+    //TODO if on the home or splash page, hide the sidebar
+    // if($location.path() == '/' || $location.path() == '/home'){
+    //   document.getElementById('sidebar').style.display = 'none';
+    // }
+
+
+    //TODO hide sidebar when window is scrolled
+    // window.onscroll = function() {
+    //   sidebar.style.display = 'none';
+    // };
+
+}
